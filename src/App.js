a3ed1f4d6612425ed18ba5c305b3b9a7e75eda57
Jonathan Morales <johnmorales990@gmail.com>
2019-09-04 11:15:50 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,33 @@
 import React, { Component } from 'react';
-import './App.css';
-
-//Rotas
 import Inicio from './components/inicio';
 import Sobre from './components/sobre';
 import Contato from './components/contato';
 import Notfound from './components/notfound';
 import Navbar from './components/navbar';
-
 import {
   BrowserRouter as Router,
   Route,
   Switch
 } from 'react-router-dom';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
         <Router>
           <div>
-          
             <Navbar></Navbar>
-          
             <Switch>
-            
               <Route path="/" exact component={Inicio}/>
               <Route path="/sobre" exact component={Sobre}/>
               <Route path="/contato/:id" exact component={Contato}/> /*Utilizando Parâmetros nas URL's*/
               <Route component={Notfound}/>
-
             </Switch>
-          
           </div>
         </Router> 
-
       </div>
-    );
+    )
   }
 }
 
