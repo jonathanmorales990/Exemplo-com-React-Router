bc0eab7caa6137d6e685e09a292fff45a065190f
Jonathan Morales <johnmorales990@gmail.com>
2019-09-04 11:11:50 -0300

    Update inicio.jsx

--- src/components/inicio.jsx
+++ src/components/inicio.jsx
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 
 class Inicio extends Component {
-
 	render() {
-  	
 		return (
 			<div>
 				<h1>Bem vindo ao Inicio</h1>
 			</div>
-
-		);
-	    
+		)
   	}
 }
 
