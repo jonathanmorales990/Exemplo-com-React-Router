743f64a70964126575ab81074d754190ed8a1ff1
Jonathan Morales <johnmorales990@gmail.com>
2019-09-04 11:11:02 -0300

    Update contato.jsx

--- src/components/contato.jsx
+++ src/components/contato.jsx
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
 
 class Sobre extends Component {
-	
 	constructor(props) {
-
 	    super(props);
-	    
-	    this.state = { id: props.match.params.id }
-
+	    this.state = { 
+		    id: props.match.params.id 
+	    }
 	}
-
 	render() {
-  	
 		return (
 			<div>
-				{this.state.id ? ( <h1>Contato: {this.state.id}</h1> ) : (<h1>Contato sem parâmetros </h1>)}
+				{
+					this.state.id ? 
+						(<h1>Contato: {this.state.id}</h1>) : (<h1>Contato sem parâmetros </h1>)
+				}
 			</div>
-
-		);
-	    
+		)
   	}
 }
 
